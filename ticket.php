ddc56e07f1d6ce271aab1b37c9cd694c308f1f16
Yeetin' Pete <github@pstar7.rf.gd>
2019-06-08 22:35:19 +0200

    friendship ended with shared hosting, now github hosting is my new friend

--- ticket.php
+++ ticket.php
@@ -1,1 +1,7 @@
-
+$videospider_movie = file_get_contents('https://videospider.in/getticket.php?key=JhhbVDPq3GIdkhuE&secret_key=a10ro5sbabnvycg0dfn1dn1h7h26bn&video_id='.$_GET["id"].'&tmdb=1&ip='.$_SERVER["REMOTE_ADDR"]); 
+$videospider_tv = file_get_contents('https://videospider.in/getticket.php?key=JhhbVDPq3GIdkhuE&secret_key=a10ro5sbabnvycg0dfn1dn1h7h26bn&video_id='.$_GET["tvid"].'&tmdb=1&s='.$_GET["season"].'&ip='.$_SERVER["REMOTE_ADDR"]);
+echo json_encode(array( 
+"ticket_mv" => $videospider_movie, 
+"ticket_tv" => $videospider_tv, 
+)); 
+?>
